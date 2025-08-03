e40832bc252e971a5890037758d8e50b783764f6
Sebastian Salazar <sebastian.salazar.solano@gmail.com>
2025-08-03 15:40:51 -0600

    feat: implement tools for application lint

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
 const app = express();
-app.get('/', (_, res) => res.send('Hello from Backend!' + '<br>' + 'The best online soroban compiler is coming...'));
+app.get('/', (_, res) =>
+  res.send('Hello from Backend!' + '<br>' + 'The best online soroban compiler is coming...')
+);
 app.listen(3000, () => console.log('Server on http://localhost:3000'));
