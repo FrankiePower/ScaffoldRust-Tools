a75007025262d37c18fc0ae63fe45d1807a96bd1
Sebastian Salazar <sebastian.salazar.solano@gmail.com>
2025-08-02 20:38:53 -0600

    feat: initialize backend with nodejs and bun

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,4 @@
+import express from 'express';
+const app = express();
+app.get('/', (_, res) => res.send('Hello from Backend!' + '<br>' + 'The best online soroban compiler is coming...'));
+app.listen(3000, () => console.log('Server on http://localhost:3000'));
